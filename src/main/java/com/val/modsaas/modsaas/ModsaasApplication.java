fd8fe7a61b038a34e627462940b07e5a142037b1
Valeria-CPaz <val.paz@gmail.com>
2025-10-03 12:45:06 -0300

    first commit

---
+++ src/main/java/com/val/modsaas/modsaas/ModsaasApplication.java
@@ -0,0 +1,13 @@
+package com.val.modsaas.modsaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModsaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModsaasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/val/modsaas/modsaas/ModsaasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.val.modsaas.modsaas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModsaasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
